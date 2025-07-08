90fba0bf858b4f834c43396d0687e8244542fb01
hamzacyberpatcher <hamzaoffuture@gmail.com>
2025-07-08 23:29:23 +0500

    added multiplePlots.py

---
+++ multiplePlots.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+data_a = np.random.random(5) * 100
+data_b = np.random.random(5) * 100
+data_c = np.random.random(5) * 100
+
+# can plot multiple graphs if u want
+plt.plot(data_a, label="my life")
+plt.plot(data_b, label="my income")
+plt.plot(data_c, label="my will to live")
+
+# sets the legends
+plt.legend(loc="lower right")
+
+plt.show()
