72651ed0f6ad82fea333f90e7cb1a17ab1647f13
hamzacyberpatcher <hamzaoffuture@gmail.com>
2025-07-08 22:34:37 +0500

    added styling

--- lineChart.py
+++ lineChart.py
@@ -10,5 +10,7 @@ print(years)
 # default plot function plots a line chart
 # years is in the place for x values
 # salary is in the place for y values
-plt.plot(years, salary)
+# in "r--", 'r' represents the color red, and '--' represents that the line is going to be dashed
+# lw is used to represent line width
+plt.plot(years, salary, "r--", lw = 4)
 plt.show()
