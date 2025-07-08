a0107f2965223987c812dc3c47bfca82b1d156fa
hamzacyberpatcher <hamzaoffuture@gmail.com>
2025-07-08 23:43:27 +0500

    added subPlots.py

---
+++ subPlots.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(100)
+
+# gives 2 rows and 2 cols
+figs, axs = plt.subplots(2, 2)
+
+# first row, first col graph
+axs[0, 0].plot(x, np.sin(x))
+axs[0, 0].set_title("Sine Wave")
+
+# first row, second col graph
+axs[0, 1].plot(x, np.cos(x))
+axs[0, 1].set_title("Cosine Wave")
+
+# second row, first col graph
+axs[1, 0].plot(x, np.random.random(100))
+axs[1, 0].set_title("Random Wave")
+
+# second row, second col graph
+axs[1, 1].plot(x, np.log(x))
+axs[1, 1].set_title("Log Wave")
+
+plt.show()
