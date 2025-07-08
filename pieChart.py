075490709424425fd7928e6eb34270ce9f0bd600
hamzacyberpatcher <hamzaoffuture@gmail.com>
2025-07-08 23:02:22 +0500

    added styling

--- pieChart.py
+++ pieChart.py
@@ -5,8 +5,14 @@ import matplotlib.pyplot as plt
 labels = ["miata", "dala", "RX-8", "Murci", "GT8"]
 # their values
 sales = [1000, 2000, 200, 1500, 800]
+# sets how much the data should be pulled out from the center of the chart visually
+explodes = [0.2, 0, 0, 0, 0]
 
 # 'sales' goes in the place for the neumrical data
 # 'labels' are the labels for those repective sales
-plt.pie(sales, labels=labels)
+# 'explodes' sets the explodes values for the data set
+# 'autopct' displays the percentage of each piece in the pie
+# 'pctdistance' sets the distance of the percentage label from the center of the pie
+# 'startangle' sets the startangle of the pie chart
+plt.pie(sales, labels=labels, explode=explodes, autopct="%.1f%%", pctdistance=0.4, startangle=45)
 plt.show()
