8164e687a8c4f03aa38121a4c16e1bbb88932a64
hamzacyberpatcher <hamzaoffuture@gmail.com>
2025-07-08 23:26:15 +0500

    added legends

--- pieChart.py
+++ pieChart.py
@@ -9,10 +9,14 @@ sales = [1000, 2000, 200, 1500, 800]
 explodes = [0.2, 0, 0, 0, 0]
 
 # 'sales' goes in the place for the neumrical data
-# 'labels' are the labels for those repective sales
+# 'labels' are the labels for those repective sales, None for none
 # 'explodes' sets the explodes values for the data set
 # 'autopct' displays the percentage of each piece in the pie
 # 'pctdistance' sets the distance of the percentage label from the center of the pie
 # 'startangle' sets the startangle of the pie chart
-plt.pie(sales, labels=labels, explode=explodes, autopct="%.1f%%", pctdistance=0.4, startangle=45)
+plt.pie(sales, labels=None, explode=explodes, autopct="%.1f%%", pctdistance=0.4, startangle=45)
+
+# 'labels' sets the labels for the legends
+# 'loc' sets the location for those legends
+plt.legend(labels=labels, loc="lower right")
 plt.show()
